898bfb9aa5bf69a891b201241f4c9c93ea5bcad2
zhaoziliang <907606163@qq.com>
2020-10-13 09:40:24 +0800

    home

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input,Message} from 'element-ui'
+import { Button, Form, FormItem, Input,Message,Container,Header,Aside,Main,
+Menu,Submenu,MenuItemGroup,MenuItem
+} from 'element-ui'
 
 // 导入弹框提示组件
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
 // 挂载
 Vue.prototype.$message = Message
